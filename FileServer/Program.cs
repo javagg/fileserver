d97c4e02590aff3b42aa1e565bbc7c5e3363b74e
lilu <lilu@miteke.com>
2017-03-27 11:17:23 +0800

    Remotely accessable

--- FileServer/Program.cs
+++ FileServer/Program.cs
@@ -59,6 +59,7 @@ namespace WebApplication1
         {
             var host = new WebHostBuilder()
                 .UseKestrel()
+                .UseUrls("http://*:5000")
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
